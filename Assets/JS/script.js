7ffd6e1a13dcd9269c9c270e9f0d5ce8556b25be
VZ13 <vinicius.zachary@gmail.com>
2024-01-23 16:59:17 -0700

    added randomizer to answer options, added interval timer and function that checks if the answer is correct

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,5 @@
 var startTime = 120;
+var score = 0;
 var timer = document.getElementById('timer');
 timer.textContent = startTime;
 var highScores = document.getElementById("scores");
@@ -9,6 +10,8 @@ var buttonElArray = [];
 var paraElArray = [];
 var questionRandomizer = [];
 var counter = 0;
+var keepPlaying = false;
+var rightORwrong = 0;
 
 
 var question1 = {
@@ -29,10 +32,26 @@ var question2 = {
 var questions = [question1, question2];
 startQuiz.addEventListener('click', playQuiz);
 highScores.addEventListener('click', displayScores);
+document.addEventListener('click', function(event){
+    if((event.target.tagName=='BUTTON') && (event.target.textContent != "Start")){
+        answerCheck(event.target);
+    }
+})
+
+setInterval(function(){
+    if (keepPlaying) {
+        timer.textContent = startTime;
+        startTime--
+        
+        
+    }
+}, 1000)
 
 function playQuiz(){
+    rightORwrong = 0;
+    keepPlaying = true;
     displayQuestion();
-    counter++;
+    //counter++; need to be on success or fail reset function
 }
 
 function displayScores(){
@@ -44,17 +63,22 @@ function displayQuestion(){
     displaySection.innerHTML = '';
     paraElArray[0] = document.createElement("p");
     displaySection.appendChild(paraElArray[0]);
-    for (let i = 0; i < 4; i++) {
+
+    var testArr = Object.values(questions[counter]);
+    paraElArray[0].textContent = testArr.shift();
+    shuffleArray(testArr);
+    
+    for (let i = 0; i < testArr.length; i++) {
         buttonElArray[i] = document.createElement("button");
         displaySection.appendChild(buttonElArray[i]);
-
+        buttonElArray[i].textContent = testArr[i];
     }
 
-    paraElArray[0].textContent = questions[counter].question;
-    buttonElArray[0].textContent = questions[counter].wronganswer1
-    buttonElArray[1].textContent = questions[counter].wronganswer2
-    buttonElArray[2].textContent = questions[counter].wronganswer3
-    buttonElArray[3].textContent = questions[counter].rightanswer
+    // paraElArray[0].textContent = questions[counter].question;
+    // buttonElArray[0].textContent = questions[counter].wronganswer1
+    // buttonElArray[1].textContent = questions[counter].wronganswer2
+    // buttonElArray[2].textContent = questions[counter].wronganswer3
+    // buttonElArray[3].textContent = questions[counter].rightanswer
 
 }
 
@@ -67,3 +91,20 @@ function shuffleArray(array) {
     }
 }
 
+function answerCheck(button){
+    if (button.textContent==questions[counter].rightanswer) {
+        rightORwrong = true
+    } else {
+        rightORwrong = false
+    }
+    // must figure out way to check without it automatically running as true or false
+    // if (rightORwrong == true) {
+    //     score += 10;
+    //     counter++;
+    //     playQuiz();
+    // } if (rightORwrong == false) {
+    //     counter++
+    //     timer -= 10;
+    //     playQuiz();
+    // }
+}
